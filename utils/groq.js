4a6d135933e3c772cd0a02d68b80a0b4e843219b
Pavan <pavannp756@gmail.com>
2025-12-13 03:19:40 +0000

    use different model for photo anaylsis

--- utils/groq.js
+++ utils/groq.js
@@ -67,7 +67,7 @@ async function analyzeDebtImage(imageUrl) {
                     ]
                 }
             ],
-            model: "llama-3.2-90b-vision-preview",
+            model: "llama-3.2-11b-vision-preview",
             response_format: { type: "json_object" }
         });
 
